c8bd182fa1d1708a3782505df7634b7deebf5522
itzrez021 <rz1125.1386@gmail.com>
2026-07-27 23:45:46 +0330

    Bot View Changed

--- bots/views.py
+++ bots/views.py
@@ -1,49 +1,98 @@
-from django.shortcuts import render,get_object_or_404
-from accounts.models import User
+from django.shortcuts import get_object_or_404
+
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
-import requests
-from rest_framework.permissions import IsAuthenticated,IsAdminUser
-from .serializers import BotTokenSerializer,TelegramBotSerializer,TelegramBotCommandsSerializer,TelegramBotCommandResponseSerializer,AddCommandSerializer
+from rest_framework.permissions import IsAuthenticated
+
+from .serializers import (
+    BotTokenSerializer,
+    TelegramBotSerializer,
+    TelegramBotCommandsSerializer,
+    TelegramBotCommandResponseSerializer,
+    AddCommandSerializer,
+)
+
 from .utils import get_bot_information
-from .models import TelegramBot,CommandResponse,BotCommand
+from .models import TelegramBot, CommandResponse, BotCommand
 
 
-# Create your views here.
+class BaseBotMixin:
+
+    def get_bot(self, bot_id):
+
+        return get_object_or_404(
+            TelegramBot,
+            id=bot_id,
+            owner=self.request.user
+        )
+
+    def get_command(self, bot_id, command_id):
+
+        return get_object_or_404(
+            BotCommand,
+            id=command_id,
+            bot__id=bot_id,
+            bot__owner=self.request.user
+        )
+
+    def get_response(self, bot_id, command_id, response_id):
+
+        return get_object_or_404(
+            CommandResponse,
+            id=response_id,
+            command__id=command_id,
+            command__bot__id=bot_id,
+            command__bot__owner=self.request.user
+        )
 
 
 class GetTokenView(APIView):
+
     permission_classes = [IsAuthenticated]
-    def post(self,request):
-        serializer = BotTokenSerializer(data=requests.data)
-        serializer.is_valid(raise_exception=True)
-        token = serializer.validated_data['token']
+
+    def post(self, request):
+
+        serializer = BotTokenSerializer(
+            data=request.data
+        )
+
+        serializer.is_valid(
+            raise_exception=True
+        )
+
+        token = serializer.validated_data["token"]
+
         data = get_bot_information(token)
+
         if not data:
+
             return Response(
                 {
-                    "error":"Invalid token."
+                    "error": "Invalid token."
                 },
                 status=status.HTTP_400_BAD_REQUEST
             )
+
         bot_id = data["id"]
-
         username = data["username"]
-
         name = data["first_name"]
-        bot = TelegramBot.objects.filter(bot_id=bot_id).first()
+
+        bot = TelegramBot.objects.filter(
+            bot_id=bot_id
+        ).first()
+
+        if bot and bot.owner == request.user:
+
+            return Response(
+                {
+                    "error":
+                    "You have already added this bot."
+                },
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
         if bot:
-            if bot.owner == request.user:
-
-                return Response(
-                    {
-                        "error":
-                        "You have already added this bot."
-                    },
-                    status=status.HTTP_400_BAD_REQUEST
-                )
-
 
             return Response(
                 {
@@ -52,35 +101,52 @@ class GetTokenView(APIView):
                 },
                 status=status.HTTP_400_BAD_REQUEST
             )
-        TelegramBot.objects.create(
+
+        bot = TelegramBot.objects.create(
 
             owner=request.user,
-
             token=token,
-
             bot_id=bot_id,
-
             username=username,
-
             name=name
 
         )
 
+        return Response(
+            {
+                "message": "Bot added successfully.",
+                "bot_id": bot.id
+            },
+            status=status.HTTP_201_CREATED
+        )
 
-class BotDetailViews(APIView):
+
+class BotDetailView(BaseBotMixin,APIView):
+
     permission_classes = [IsAuthenticated]
-    def get(self,request,bot_id):
-        bot = get_object_or_404(TelegramBot,id=bot_id,owner=request.user)
+
+    def get(self, request, bot_id):
+
+        bot = self.get_bot(bot_id)
+
         commands = bot.commands.all()
-        return Response({"bot":TelegramBotSerializer(bot).data,
-                        "commands":TelegramBotCommandsSerializer(commands,many=True).data})
+
+        return Response(
+            {
+                "bot":
+                TelegramBotSerializer(bot).data,
+
+                "commands":
+                TelegramBotCommandsSerializer(
+                    commands,
+                    many=True
+                ).data
+            }
+        )
+
     def post(self, request, bot_id):
 
-        bot = get_object_or_404(
-            TelegramBot,
-            id=bot_id,
-            owner=request.user
-        )
+        bot = self.get_bot(bot_id)
 
         serializer = AddCommandSerializer(
             data=request.data,
@@ -106,11 +172,30 @@ class BotDetailViews(APIView):
         )
 
 
+class BotCommandDetailView(BaseBotMixin,APIView):
 
-class BotCommandDetailView(APIView):
     permission_classes = [IsAuthenticated]
-    def get(self,request,bot_id,command_id):
-        command = get_object_or_404(BotCommand,id=command_id,bot__id=bot_id,bot__owner=request.user)
+
+    def get(self, request, bot_id, command_id):
+
+        command = self.get_command(
+            bot_id,
+            command_id
+        )
+
         responses = command.responses.all()
-        return Response({"command":TelegramBotCommandsSerializer(command).data,
-                            "command_responses":TelegramBotCommandResponseSerializer(responses,many=True).data})
+
+        return Response(
+            {
+                "command":
+                TelegramBotCommandsSerializer(
+                    command
+                ).data,
+
+                "command_responses":
+                TelegramBotCommandResponseSerializer(
+                    responses,
+                    many=True
+                ).data
+            }
+        )
